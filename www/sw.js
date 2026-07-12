0cf3d56262cfb8fec1c4f1c4a44cff174d938e71
MrTh0m <illienthomas@gmail.com>
2026-07-12 12:08:22 +0200

    clean

--- www/sw.js
+++ www/sw.js
@@ -6,7 +6,7 @@
  *   - API (api.php, proxy.php) : réseau d'abord, cache en secours
  */
 
-const SHELL_VER = 'emmgo-shell-v35';
+const SHELL_VER = 'emmgo-shell-v37';
 const DATA_VER  = 'emmgo-data-v4';
 const ALL_CACHES = [SHELL_VER, DATA_VER];
 
